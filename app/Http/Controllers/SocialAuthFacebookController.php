53d25348e49521e3d4661f7fb476fa444ca98305
KrunalLathiya <krunallathiya21@gmail.com>
2017-07-13 14:26:56 +0530

    Laravel 5.4 Facebook Login

---
+++ app/Http/Controllers/SocialAuthFacebookController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Socialite;
+use App\Services\SocialFacebookAccountService;
+
+class SocialAuthFacebookController extends Controller
+{
+  /**
+   * Create a redirect method to facebook api.
+   *
+   * @return void
+   */
+    public function redirect()
+    {
+        return Socialite::driver('facebook')->redirect();
+    }
+
+    /**
+     * Return a callback method from facebook api.
+     *
+     * @return callback URL from facebook
+     */
+    public function callback(SocialFacebookAccountService $service)
+    {
+        $user = $service->createOrGetUser(Socialite::driver('facebook')->user());
+        auth()->login($user);
+        return redirect()->to('/home');
+    }
+}
